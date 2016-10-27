757ef554e5d379bd09888e4723f04a25da2285e5
caoxp <155137572@qq.com>
2016-10-27 18:41:28 +0800

    D

--- motor_Controller.cpp
+++ motor_Controller.cpp
@@ -197,7 +197,7 @@ void homeYaxis()
     }
 
 
-    stepperY.move(  0 -320  *  Y_STEP_PER_MM);        //without loop
+    stepperY.move(  0 -200  *  Y_STEP_PER_MM);        //without loop
     stepperY.runToPosition();
 
     stepperY.setCurrentPosition(0);     //Clear the last steps
